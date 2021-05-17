1d8cbcf3219a380dc0af85a25f79144cc5481bd5
dellek1 <dellek@interia.pl>
2021-05-17 16:59:33 +0200

    s

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- szerokość urządzenia, bez zooma -->
+	
+	<title>Polskie skoki</title>
+	<meta name="description" content="Skoki narciarskie">
+	<meta name="keywords" content="skoki">
+	<meta http-equiv="X-Ua-Compatible" content="IE=edge">
+	
+	<link rel="stylesheet" href="css/bootstrap.min.css"> <!-- arkusz css bootstrapa -->
+	<link rel="stylesheet" href="main.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+	
+	<!--[if lt IE 9]>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<![endif]-->
+	
+</head>
+
+<body>
+	<header>
+		<nav class="navbar navbar-dark bg-jumpers navbar-expand-lg"> <!-- start <nav> -->
+		
+			<a class="navbar-brand"href="index_start.php"><img src="img/logo.png" width="60" height="50" class="d-inline-block mr-1 align-bottom" alt=""> </a><b> SKOKI.PL</b>				
+			<div class="collapse navbar-collapse" id="menu" aria-controls="menu" aria-expanded="false" aria-label="Przełącznik nawigacji">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link" href="#"> Start </a>
+					</li>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-extended="false" id="submenu" aria-haspopup="true"> Zawody </a>
+						
+						<div class="dropdown-menu" aria-labelledby="submenu">
+							<a class="dropdown-item" href="#"> Terminarz zawodów </a>
+							<a class="dropdown-item" href="#"> Ranking Pucharu Świata </a>
+							
+							<div class="dropdown-devider"></div>
+							
+							<a class="dropdown-item" href="#"> Sylwetki zawodników </a>
+							<a class="dropdown-item" href="#"> Skocznie narciarskie </a>
+						</div>
+					<li class="nav-item dropdown">
+						<a class="nav-link" href="#"> Historia </a>
+					</li>
+										<li class="nav-item active">
+						<a class="nav-link" href="index.html"> Zdjęcia </a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link" href="#"> Wywiady </a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="Kontakt.html"> Kontakt </a>
+					</li>
+				</ul>
+				<form class="form-inline">
+					<a style="color: black" class="btn btn-light" class="nav-link" href="Logowanie.php"> Logowanie </a>
+					<a style="color: black" class="btn btn-light ml-2" class="nav-link" href="wyszukaj.php"> Wyszukaj </a>
+				</form>
+			</div>	
+		</nav> 	<!-- koniec </nav> -->
+	</header>
+	<main>
+		
+		<section class="jumpers"> <!-- start <section> -->
+		
+			<div class ="container"> <!-- start container -->
+
+				<header>
+					
+					<h1>Skoczkowie narciarscy</h1>
+					<p>Na tej stronie prezentujemy sylwetki popularnych skoczków narciarskich, których znamy z rywalizacji w konkursach pucharu olimpijskiego oraz pucharu świata. Kliknij na swojego ulubionego skoczka by dowiedzieć się więcej.</p>
+						
+				</header>
+				
+				<div class="row"> <!-- start wiersz siatki -->
+					
+					<div class="col-sm-6 col-md-5 offset-md-1"> 
+					
+						<figure>
+							<a href="wpis_malysz.html"><img img class="img-thumbnail" src="img/malysz.jpg" alt="Adam Małysz"></a>
+							<figcaption>Adam Małysz</figcaption>
+						</figure>
+					
+					</div>
+					
+					<div class="col-sm-6 col-md-5">
+					
+						<figure> <!-- obraz -->
+							<a href="wpis_stoch.html"><img img class="img-thumbnail" src="img/stoch.jpg" alt="Kamil Stoch"></a>
+							<figcaption>Kamil Stoch</figcaption>
+						</figure>
+					
+					</div>
+
+					<div class="col-sm-6 col-md-4"> <!-- 1/3 ekranu medium devices -->
+					
+						<figure>
+							<a href="wpis_zyla.html"><img img class="img-thumbnail" src="img/zyla.jpg" alt="Piotr Żyła"></a>
+							<figcaption>Piotr Żyła</figcaption>
+						</figure>
+					
+					</div>
+
+					<div class="col-sm-6 col-md-4">
+					
+						<figure>
+							<a href="wpis_schmitt.html"><img img class="img-thumbnail" src="img/schmitt.jpg" alt="Martin Schmitt"></a>
+							<figcaption>Martin Schmitt</figcaption>
+						</figure>
+					
+					</div>
+
+					<div class="col-sm-6 col-md-4">
+					
+						<figure>
+							<a href="wpis_kubacki.html"><img img class="img-thumbnail" src="img/kubacki.jpg" alt="Dawid Kubacki"></a>
+							<figcaption>Dawid Kubacki</figcaption>
+						</figure>
+					
+					</div>
+
+					<div class="col-sm-6 col-md-4">
+						<figure>
+							<a href="wpis_ahonen.html"><img img class="img-thumbnail" src="img/ahonen.jpg" alt="Janne Ahonen"></a>
+							<figcaption>Janne Ahonen</figcaption>
+						</figure>
+					
+					</div>
+
+					<div class="col-sm-6 col-md-4">
+					
+						<figure>
+							<a href="wpis_kranjec.html"><img img class="img-thumbnail" src="img/kranjec.jpg" alt="Robert Kranjec"></a>
+							<figcaption>Robert Kranjec</figcaption>
+						</figure>
+					
+					</div>
+
+					<div class="col-sm-6 col-md-4">
+					
+						<figure>
+							<a href="wpis_schliri.html"><img img class="img-thumbnail" src="img/schlierenzauer.jpg" alt="Gregor Schlierenzauer"></a>
+							<figcaption>Gregor Schlierenzauer</figcaption>
+						</figure>
+					
+					</div>
+					
+					<div class="col-sm-6 col-md-3">
+					
+						<figure>
+							<a href="wpis_kot.html"><img img class="img-thumbnail" src="img/kot.jpg" alt="Maciej Kot"></a>
+							<figcaption>Maciej Kot</figcaption>
+						</figure>
+					
+					</div>
+					
+					<div class="col-sm-6 col-md-3">
+					
+						<figure>
+							<a href="wpis_kraft.html"><img img class="img-thumbnail" src="img/kraft.jpg" alt="Stefan Kraft"></a>
+							<figcaption>Stefan Kraft</figcaption>
+						</figure>
+					
+					</div>					
+					
+					<div class="col-sm-6 col-md-3">
+					
+						<figure>
+							<a href="wpis_ammann.html"><img img class="img-thumbnail" src="img/ammann.jpg" alt="Simon Ammann"></a>
+							<figcaption>Simon Ammann</figcaption>
+						</figure>
+					
+					</div>						
+					
+					<div class="col-sm-6 col-md-3">
+					
+						<figure>
+							<a href="wpis_kasai.html"><img img class="img-thumbnail" src="img/kasai.jpg" alt="Noriaki Kasai"></a>
+							<figcaption>Noriaki Kasai</figcaption>
+						</figure>
+					
+					</div>						
+				</div>
+			</div>	<!-- koniec container -->
+				<footer>
+            <div class="container">
+                <div class="top-footer">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6">
+                            <div class="widget widget-about">
+                                <img src="images/logo.png" alt="">
+                                <p>SKOKOWNIA  <br/> ul. Skokowa 20 lok. 4</br>99-999 Opole</br></p>
+                                
+                                   <span>Tel. 602-120-987</span></br>
+                                   <a href="mailto:example@example.com" title="">testowa@gmail.com</a>
+                                
+                            </div><!--widget-about end-->
+                        </div>
+                        <div class="col-md-2 col-sm-6">
+                            <div class="widget widget-links">
+                                <h4 class="widget-title">SPOŁECZNOŚĆ</h4>
+                                <ul>
+                                    <li><a href="#" title="">Twitter</a></li>
+                                    <li><a href="#" title="">Linkedin</a></li>
+                                    <li><a href="#" title="">Instagram</a></li>
+                                    <li><a href="#" title="">Facebook</a></li>
+                                </ul>
+                            </div><!--widget-links end-->
+                        </div>
+                        <div class="col-md-2 col-sm-6">
+                            <div class="widget widget-links">
+                                <h4 class="widget-title">FIRMA</h4>
+                                <ul>
+                                        <li><a href="about.html" title="">O mnie</a></li>
+                                        <li><a href="team.html" title="">Moja strona</a></li>
+                                        <li><a href="prices.html" title="">Cennik</a></li>
+                                        <li><a href="contact.html" title="">Kontakt</a></li>
+                                </ul>
+                            </div><!--widget-links end-->
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="widget widget-links">
+                                <h4 class="widget-title">USŁUGI</h4>
+                                <ul>
+                                        <li><a href="services.html" title="">Olimpiady</a></li>
+                                        <li><a href="services.html" title="">Zdjęcia skoczków</a></li>
+                                        <li><a href="services.html" title="">Pamiątki</a></li>
+                                        <li><a href="services.html" title="">Dotacje</a></li>
+                                </ul>
+                            </div><!--widget-links end-->
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom-strip">
+                    <ul class="bt-links">
+                        <li><a href="#" title="">Polityka prywatności</a></li>
+                        <li><a href="#" title="">Regulamin</a></li>
+						<li><p>© 2021 Caterpillar. All Rights Reserved.</p></li>
+                    </ul><!--bt-links end-->
+					
+                </div>
+            </div>
+        </footer><!--footer end-->
+		</section>
+		
+	</main>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
